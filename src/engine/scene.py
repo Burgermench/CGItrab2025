bd759c19a0f95c23769ce9600492ccdf335cea38
Burgermench <jmc.work.co@gmail.com>
2025-05-22 18:25:48 +0100

    compounding basic functions of engine
    Completed src/engine/scene.update function
    Added new src/engine/scene.render function

--- src/engine/scene.py
+++ src/engine/scene.py
@@ -14,13 +14,26 @@ class GameScene:
         self.player = Player()
         self.effects = EffectManager()
         self.lanes = [NoteLane(i, key) for i, key in enumerate(self.player.keymap)]
-
-    # notecharts + music
-    self.chart = load_chart("charts/note_chart.json")
-    self.song_start_time = pygame.time.get_ticks() / 1000
-    self.song_started = True
-    
-    # spawn notes based on music time
-    current_time = (pygame.time.get_ticks() - self.song_start_time) / 1000.0
-    while self.chart_index < len(self.chart) and self.chart[self.chart_index]["time"] <= current_time:
-        note_data
+        self.chart = load_chart("charts/notechart.json")
+        self.chart_index = 0
+        self.song_started = False
+        self.song_start_time = 0
+        
+    def update(self, dt):
+        # start music when game starts
+        if not self.song_started:
+            play_music("assets/sounds/song.mp3")
+            self.song_start_time = pygame.time.get_ticks()
+            self.song_started = True
+            
+        # spawn notes based on music timing
+        current_time = (pygame.time.get_ticks() - self.song_start_time) / 1000.0
+        while self.chart_index < len(self.chart) and self.chart[self.chart_index]["time"] <= current_time:
+            note_data = self.chart[self.chart_index]
+            self.lanes[note_data["lane"]].spawn_note(note_data["instrument"])
+            self.chart_index += 1
+            
+        for lane in self.lanes:
+            lane.update(dt, self.player)
+            
+    def render(self):
